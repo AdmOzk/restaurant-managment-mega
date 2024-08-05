ecbc9ba5f5aa31d3cafa04645fb2f46a1df73d1b
AdmOzk <adem.ozkyt@gmail.com>
2024-08-05 12:04:36 +0300

    CleanCode commit

--- src/components/WaiterScreen/WaiterScreen.ts
+++ src/components/WaiterScreen/WaiterScreen.ts
@@ -1,10 +1,18 @@
 import { defineComponent } from 'vue';
 import PaymentButton from '../PaymentButton/PaymentButton.vue';
+import OrderButton from '../OrderButton/OrderButton.vue';
+import CalendarButton from '../CalendarButton/CalendarButton.vue';
+import PersonalInfoButton from '../PersonalInfoButton/PersonalInfoButton.vue';
+import MessageBoxButton from '../MessageBoxButton/MessageBoxButton.vue';
 
 export default defineComponent({
   name: 'WaiterScreen',
   components:{
-    PaymentButton
+    PaymentButton,
+    OrderButton,
+    CalendarButton,
+    PersonalInfoButton,
+    MessageBoxButton
   }
   // TS tarafı buraya
 });
